a892de169d7d0d40be79821134f7e0380fd5c509
Stefan Wieczorek <stefan.wieczorek@mgt-commerce.com>
2020-04-02 18:08:07 +0200

    .

---
+++ cloudpanel-ce/docusaurus.config.js
@@ -0,0 +1,96 @@
+module.exports = {
+  title: 'CloudPanel CE | Documentation',
+  tagline: 'CloudPanel CE | Documentation',
+  url: 'https://github.com/cloudpanel-io/docs/tree/master/docs/cloudpanel-ce',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'CloudPanel',
+  projectName: 'CloudPanelCE',
+  themeConfig: {
+    disableDarkMode: true,
+    navbar: {
+      title: 'CloudPanel CE',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      links: [
+        {
+          to: 'docs/doc1',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {
+          href: 'https://github.com/cloudpanel-io/docs/tree/master/docs/cloudpanel-ce',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/doc1',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'Social',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
