70888d771f01473aeed9e78209b0e3483fef3e7d
Stefan Wieczorek <stefan.wieczorek@mgt-commerce.com>
2020-10-14 15:46:43 +0200

    .

--- cloudpanel-ce/old/sync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -f /Users/$(whoami)/.ssh/known_hosts
-#rm -rf /Users/$(whoami)/Library/Application\ Support/Unison/*
-unison -prefer newer -ignore "Name .git" -ignore "Path var" -repeat watch /Users/admin/PhpstormProjects/cloudpanel-docs/ ssh://root@127.0.0.1//home/cloudpanel/htdocs/cloudpanel-docs.clp/ -ui text
